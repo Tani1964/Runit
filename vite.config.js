5d8ffee85b6412440ad03c8c360ea003cdbc6748
Tani1964 <ifegbesan6@gmail.com>
2024-10-23 15:21:59 +0100

    changed base url to repo name

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
-  base: "/Runit/",
+  base: "/web_v2/",
   define: {
     'process.env': {}, // Useful if some libraries depend on `process.env`
   },
